ba09988351da4d80e20a818aa07b5ddf7c58236a
unknown <unlugokhanugur@gmail.com>
2020-03-16 14:21:29 +0200

    Second Commit

--- src/gitProject/Git_Project01.java
+++ src/gitProject/Git_Project01.java
@@ -1,5 +1,12 @@
 package gitProject;
 
-public class Git_Project01 {
 
+
+public class Git_Project01 {
+public static void main(String[]args) {
+	
+	int a=3;
+	int b=4;
+	System.out.println(a+b);
+}
 }
